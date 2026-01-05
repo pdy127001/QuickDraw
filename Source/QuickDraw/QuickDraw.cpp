c15f26d0a3d78ee75043e5fddf5f6a5bb2fca9de
PDY <pdy127001@gmail.com>
2026-01-05 20:36:57 +0900

    Initial commit

---
+++ Source/QuickDraw/QuickDraw.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "QuickDraw.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickDraw, "QuickDraw" );
